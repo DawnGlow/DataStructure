bd8567a3d3ee3403e68dbc3478b5e6419ea8e253
DawnGlow <ferrater1013@gmail.com>
2023-10-02 23:44:03 +0900

    CLinkedList changed
    CLinkedList::Remove Function changed(considering tail)

--- Code_by_DataStructure/CLinkedList/CLinkedList.c
+++ Code_by_DataStructure/CLinkedList/CLinkedList.c
@@ -58,9 +58,18 @@ int LNext(List *plist, Data *pdata) {
     }
 }
 
+// 틀린 이유 : tail이 삭제되는 경우 tail을 옮겨야 하는 것을 고려하지 않았음
 Data LRemove(List *plist) {
     Data result = (plist->cur)->data;
     plist->before->next = plist->cur->next;
+    if (plist->cur == plist->tail) { // 꼬리인지 확인
+        if (plist->tail == plist->tail->next) { // 마지막 노드라면
+            plist->tail = NULL;
+        }
+        else { // 마지막 노드가 아니라면
+            plist->tail = plist->before; // 꼬리를 한칸 앞을 가리키게 이동
+        }
+    }
     free(plist->cur);
     plist->cur = plist->before;
     (plist->numOfData)--;
